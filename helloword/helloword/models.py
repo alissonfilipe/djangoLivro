cca9af1105d8226aa42ef9efdd1edb926a66c619
Alisson <alisson.santos4@estudante.ifto.edu.br>
2024-06-05 17:53:13 -0300

    tentando 1

---
+++ helloword/helloword/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Funcionario(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    sobrenome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+
